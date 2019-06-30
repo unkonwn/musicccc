90b7a3904e2ca0ff4941d3989062ec53d6eb85af
MACHENIKE <MACHENIKE@169.254.117.243>
2019-06-30 21:09:24 +0800

    test

---
+++ src/com/musicweb/dao/userInfoDao.java
@@ -0,0 +1,9 @@
+package com.musicweb.dao;
+
+import com.musicweb.entity.UserInformation;;
+
+public interface userInfoDao {
+
+	public boolean getUser(int userId);
+
+}
---
+++ src/com/musicweb/dao/userInfoDaoImpl.java
@@ -0,0 +1,20 @@
+package com.musicweb.dao;
+
+import org.hibernate.Session;
+
+import com.musicweb.entity.UserInformation;
+import com.musicweb.util.HibernateSessionFactory;
+
+public class userInfoDaoImpl implements userInfoDao{
+
+	@Override
+	public boolean getUser(int userId) {
+		Session session = HibernateSessionFactory.getSession();
+		session.get(user, id)
+		
+		return false;
+	}
+
+
+
+}
