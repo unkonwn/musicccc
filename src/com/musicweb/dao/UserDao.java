3bafd28c5f4a36d77f78b9fb5b8b0622323df27f
MACHENIKE <MACHENIKE@169.254.117.243>
2019-07-01 10:56:46 +0800

    dao

--- src/com/musicweb/dao/UserDao.java
+++ src/com/musicweb/dao/UserDao.java
@@ -1,13 +1,40 @@
 package com.musicweb.dao;
 
+import java.util.List;
+
 import com.musicweb.entity.*;
+import com.musicweb.util.PageItem;
 
 public interface UserDao {
-	public User getUserById(int userId);
-	public boolean createMyMusiclist(Musiclist musiclist);
-	public boolean addMusiclist(Musiclist musiclist);
-	public boolean deletMyMusiclist(Musiclist musiclist);
-	public boolean deletColMusiclist(Musiclist musiclist);
+
+	public User getUser(int userId);
 	
+	public List<PageItem> getCollectedMusicLists(int pageNum, int pageSize, String sortWay, String sortItem);
+
+	public List<PageItem> getMyMusicLists(int pageNum, int pageSize, String sortWay, String sortItem);
+
+	public List<PageItem> getComments(int pageNum, int pageSize, String sortWay, String sortItem);
+
+	public boolean deletComment(Comment comment);
+
+	public boolean addComment(Comment comment);
+
+	public boolean updateUser(User user);
+
+	public boolean deletUser(int userId);
+
+	public boolean createMyMusicList(Musiclist music);
+
+	public boolean addCollectedMusicList(Musiclist music);
+
+	public boolean deletMyMusicList(Musiclist music);
+
+	public boolean deletCreatedMusicList(Musiclist music);
+
+	public boolean updatePassword(String password);
+
+	public boolean updateHeadImage(String url);
+
+	public boolean insertUser(User user);
 
 }
--- src/com/musicweb/dao/UserDaoImpl.java
+++ src/com/musicweb/dao/UserDaoImpl.java
@@ -1,86 +1,105 @@
 package com.musicweb.dao;
 
+import java.util.List;
+
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.musicweb.entity.*;
 import com.musicweb.util.HibernateSessionFactory;
+import com.musicweb.util.PageItem;
 
-public class UserDaoImpl implements UserInfoDao{
+public class UserDaoImpl implements UserDao{
 
-    public Transaction transaction = new Transaction();
+    public Transaction transaction;
 
 
+    @Override
     public User getUser(int userId) {
         // TODO implement here
         return null;
     }
 
-    public boolean createMyMusicList(MusicList music) {
+    @Override
+    public boolean createMyMusicList(Musiclist music) {
         // TODO implement here
         return false;
     }
 
-    public boolean addCollectedMusicList(MusicList music) {
+    @Override
+    public boolean addCollectedMusicList(Musiclist music) {
         // TODO implement here
         return false;
     }
 
-    public boolean deletMyMusicList(MusicList music) {
+    @Override
+    public boolean deletMyMusicList(Musiclist music) {
         // TODO implement here
         return false;
     }
 
-    public boolean deletMyMusicList(MusicList music) {
+    @Override
+    public boolean deletCreatedMusicList(Musiclist music) {
         // TODO implement here
         return false;
     }
 
+    @Override
     public boolean updatePassword(String password) {
         // TODO implement here
         return false;
     }
 
+    @Override
     public boolean updateHeadImage(String url) {
         // TODO implement here
         return false;
     }
 
+    @Override
     public boolean insertUser(User user) {
         // TODO implement here
         return false;
     }
 
+    @Override
     public boolean deletUser(int userId) {
         // TODO implement here
         return false;
     }
 
+    @Override
     public boolean updateUser(User user) {
         // TODO implement here
         return false;
     }
 
+    @Override
     public boolean addComment(Comment comment) {
         // TODO implement here
         return false;
     }
 
+    @Override
     public boolean deletComment(Comment comment) {
         // TODO implement here
         return false;
     }
 
-    public pageUtil getComments(int pageNum, int pageSize, String sortWay, String sortItem) {
+    @Override
+    public List<PageItem> getComments(int pageNum, int pageSize, String sortWay, String sortItem) {
         // TODO implement here
         return null;
     }
 
-    public pageUtil getMyMusicLists(int pageNum, int pageSize, String sortWay, String sortItem) {
+    @Override
+    public List<PageItem> getMyMusicLists(int pageNum, int pageSize, String sortWay, String sortItem) {
         // TODO implement here
         return null;
     }
 
-    public pageUtil getCollectedMusicLists(int pageNum, int pageSize, String sortWay, String sortItem) {
+    @Override
+    public List<PageItem> getCollectedMusicLists(int pageNum, int pageSize, String sortWay, String sortItem) {
         // TODO implement here
         return null;
     }
